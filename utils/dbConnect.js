8db187868c32e72a69014e774ebf5de600577320
icicleltd <shihabicicle@gmail.com>
2026-03-29 16:05:43 +0600

    refine db connect fun

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -1,35 +1,65 @@
-const { default: mongoose } = require("mongoose");
+// const { default: mongoose } = require("mongoose");
 
-// const connectDb = async () => {
+// // const connectDb = async () => {
+// //   try {
+// //     await mongoose.connect(process.env.MONGO_URL);
+// //     console.log("MongoDB Connected Successfully");
+// //   } catch (error) {
+// //     throw error;
+// //   }
+// // };
+
+
+
+
+// let isConnected = false;
+
+//  const connectDB = async () => {
+//   if (isConnected) {
+//     // Already connected
+//     return;
+//   }
+
 //   try {
-//     await mongoose.connect(process.env.MONGO_URL);
-//     console.log("MongoDB Connected Successfully");
+//     const db = await mongoose.connect(process.env.MONGO_URL, {
+//       bufferCommands: false,
+//     });
+
+//     isConnected = db.connections[0].readyState === 1;
+//     console.log("MongoDB connected");
 //   } catch (error) {
-//     throw error;
+//     console.error("MongoDB connection failed:", error);
+//     // throw new AppError(500, "Mongodb connect error");
 //   }
 // };
+// module.exports = { connectDB };
 
 
+const mongoose = require("mongoose");
 
+let cached = global.mongoose;
 
-let isConnected = false;
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
 
- const connectDB = async () => {
-  if (isConnected) {
-    // Already connected
-    return;
+const connectDB = async () => {
+  if (cached.conn) {
+    return cached.conn;
   }
 
-  try {
-    const db = await mongoose.connect(process.env.MONGO_URL, {
+  if (!cached.promise) {
+    if (!process.env.MONGO_URL) {
+      throw new Error("MONGO_URL is missing");
+    }
+
+    cached.promise = mongoose.connect(process.env.MONGO_URL, {
       bufferCommands: false,
     });
-
-    isConnected = db.connections[0].readyState === 1;
-    console.log("MongoDB connected");
-  } catch (error) {
-    console.error("MongoDB connection failed:", error);
-    // throw new AppError(500, "Mongodb connect error");
   }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
 };
+
 module.exports = { connectDB };
